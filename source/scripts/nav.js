f419dadc4027e61040a59f4506bc4a2944908bcd
Ryzen-71 <super.ya-prio237@ya.ru>
2024-09-06 19:08:08 +0300

    Сделал мобильную версию  главной страницы

--- source/scripts/nav.js
+++ source/scripts/nav.js
@@ -12,17 +12,17 @@
 // });
 
 
-const navElement = document.querySelector('.main-header__nav');
-const buttonElement = document.querySelector('.main-header__toggler');
+// const navElement = document.querySelector('.main-header__nav');
+// const buttonElement = document.querySelector('.main-header__toggler');
 
-navElement.classList.remove('main-header__nav--closed');
+// navElement.classList.remove('main-header__nav--closed');
 
-buttonElement.addEventListener('click', () => {
-  if (navElement.classList.contains('main-header__nav--closed')) {
-    navElement.classList.add('main-header__nav--opened');
-    navElement.classList.remove('main-header__nav--closed');
-  } else {
-    navElement.classList.add('main-header__nav--closed');
-    navElement.classList.remove('main-header__nav--opened');
-  }
-});
+// buttonElement.addEventListener('click', () => {
+//   if (navElement.classList.contains('main-header__nav--closed')) {
+//     navElement.classList.add('main-header__nav--opened');
+//     navElement.classList.remove('main-header__nav--closed');
+//   } else {
+//     navElement.classList.add('main-header__nav--closed');
+//     navElement.classList.remove('main-header__nav--opened');
+//   }
+// });
