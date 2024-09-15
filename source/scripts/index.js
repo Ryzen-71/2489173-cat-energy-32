1652b6494c0b309ab4e7579c9c42cd6e8b0f8058
Ryzen-71 <super.ya-prio237@ya.ru>
2024-09-15 16:56:40 +0300

    Испавил недочеты

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,15 +1,31 @@
 /* в этот файл добавляет скрипты*/
+
 const navElement = document.querySelector('.main-header__nav');
 const buttonElement = document.querySelector('.main-header__toggler');
 
-navElement.classList.add('main-header__nav--closed');
+navElement.classList.add('main-header__nav--opened');
 
 buttonElement.addEventListener('click', () => {
-  if (navElement.classList.contains('main-header__nav--closed')) {
-    navElement.classList.remove('main-header__nav--closed');
-    navElement.classList.add('main-header__nav--opened');
-  } else {
+  if (navElement.classList.contains('main-header__nav--opened')) {
     navElement.classList.remove('main-header__nav--opened');
     navElement.classList.add('main-header__nav--closed');
+  } else {
+    navElement.classList.remove('main-header__nav--closed');
+    navElement.classList.add('main-header__nav--opened');
   }
 });
+
+// const navElement = document.querySelector('.main-header__nav');
+// const buttonElement = document.querySelector('.main-header__toggler');
+
+// navElement.classList.add('main-header__nav--closed');
+
+// buttonElement.addEventListener('click', () => {
+//   if (navElement.classList.contains('main-header__nav--closed')) {
+//     navElement.classList.remove('main-header__nav--closed');
+//     navElement.classList.add('main-header__nav--opened');
+//   } else {
+//     navElement.classList.remove('main-header__nav--opened');
+//     navElement.classList.add('main-header__nav--closed');
+//   }
+// });
