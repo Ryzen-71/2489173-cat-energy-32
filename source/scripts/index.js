6f25abfe861285aa3a29cf1cdc59fd1603ba14c0
Ryzen-71 <super.ya-prio237@ya.ru>
2024-09-21 14:04:57 +0300

    Внес поправки для проверки перед защитой

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -5,7 +5,7 @@ const buttonElement = document.querySelector('.main-header__toggler');
 
 
 navElement.classList.remove('main-header__nav--nojs');
-navElement.classList.add('main-header__nav--opened');
+navElement.classList.add('main-header__nav--closed');
 
 buttonElement.addEventListener('click', () => {
   if (navElement.classList.contains('main-header__nav--opened')) {
